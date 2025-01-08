5e5eef4f50cba3f6c8008bcc120f761eadc2b9bb
Rodrigo Menezes <rodriisabatico@gmail.com>
2025-01-08 15:13:43 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideosAPI.Models;
+
+namespace VideosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VideoController : ControllerBase
+{
+    private static List<Video> videos = new List<Video>();
+
+    [HttpPost]
+    public IActionResult AdicionaVideo([FromBody] Video video)
+    {
+        videos.Add(video);
+        return Ok();
+    }
+
+    [HttpGet]
+    public IEnumerable<Video> RecuperaVideos()
+    {
+        return videos;
+    }
+}
---
+++ Models/Video.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideosAPI.Models;
+
+public class Video
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do vídeo é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição do vídeo é obrigatória")]
+    [MaxLength(100, ErrorMessage = "O tamanho da descrição do vídeo não pode passar de 100 caracteres")]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "A URL é obrigatória")]
+    [DataType(DataType.Url)]
+    public string Url { get; set; }
+}
